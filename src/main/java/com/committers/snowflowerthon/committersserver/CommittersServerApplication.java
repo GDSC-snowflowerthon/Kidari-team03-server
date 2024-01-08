9c054c436306eba32807162615a59c223aad3780
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-01-08 16:10:04 +0900

    🎉 프로젝트 시작
    스프링부트 파일을 생성합니다.

---
+++ src/main/java/com/committers/snowflowerthon/committersserver/CommittersServerApplication.java
@@ -0,0 +1,13 @@
+package com.committers.snowflowerthon.committersserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommittersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommittersServerApplication.class, args);
+	}
+
+}
