3e7d8b5a1c351293194d2690b640d82604125ca4
leeseohyun <applemint2003@gmail.com>
2024-01-10 17:23:31 +0900

    Revert "Merge branch 'dev/sh' into feat/github/dev"
    This reverts commit 4b25941c2c57e5d1cb06514febf18b67ce719d8f, reversing
    changes made to fbf70114973c1d1b38ecbb47559b39614fe0271c.

--- src/main/java/com/committers/snowflowerthon/committersserver/domain/member/controller/MemberController.java
+++ src/main/java/com/committers/snowflowerthon/committersserver/domain/member/controller/MemberController.java
@@ -1,49 +1,7 @@
 package com.committers.snowflowerthon.committersserver.domain.member.controller;
 
-import com.committers.snowflowerthon.committersserver.domain.member.dto.MemberOtherResDto;
-import com.committers.snowflowerthon.committersserver.domain.member.dto.MemberOwnResDto;
-import com.committers.snowflowerthon.committersserver.domain.member.dto.MemberSearchResDto;
-import com.committers.snowflowerthon.committersserver.domain.member.entity.Member;
-import com.committers.snowflowerthon.committersserver.domain.member.service.MemberService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1")
 public class MemberController {
-
-    private final MemberService memberService;
-
-    /*
-    // 내 정보 받아오는 부분 필요
-     */
-    
-    @PatchMapping("/home/growth")
-    public void growSnowman()
-        Member member = memberService.getMemberById(id);
-        memberService.growSnowman(id);
-    }
-
-    @GetMapping("/buddy/search") // 유저 검색
-    public ResponseEntity<MemberSearchResDto> searchMember(@RequestParam String nickname) {
-        Member member = memberService.getMemberByNickname(nickname);
-        if (member == null) {
-            return ResponseEntity.notFound().build();
-        }
-        MemberSearchResDto searchedMember = MemberSearchResDto.toDto(member);
-        return ResponseEntity.ok(searchedMember);
-    }
-
-    @GetMapping("/user") // 유저 정보 페이지 조회
-    public ResponseEntity<MemberOtherResDto> getMemberInfo(@RequestParam String nickname) {
-        Member member = memberService.getMemberByNickname(nickname);
-        if (member == null) {
-            return ResponseEntity.notFound().build();
-        }
-        MemberOtherResDto otherMember = MemberOtherResDto.toDto(member);
-        return ResponseEntity.ok(otherMember);
-    }
-
 }
