dee44e096f3ed9f50dcf131b95929a122b68af13
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-01-11 17:23:13 +0900

    🔒 HTTPS 설정
    - 로드밸런싱 및 CI/CD 테스트를 위한 푸시입니다.

--- src/main/java/com/committers/snowflowerthon/committersserver/CommittersServerApplication.java
+++ src/main/java/com/committers/snowflowerthon/committersserver/CommittersServerApplication.java
@@ -3,7 +3,6 @@ package com.committers.snowflowerthon.committersserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
