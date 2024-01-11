f65884512a41d20d6b61b9f7b69b1c17c3652c3d
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-01-12 04:06:36 +0900

    ✨ 로그인 후 커밋 개수 갱신 구현
    - 로그인 시 커밋 개수가 카운트 되는 로직을 구현했습니다.
    - 추후 본인 레포가 아닌 오거 레포에서의 기여도 카운트 되는 로직 구현을 추가하면 좋을 듯 합니다.

--- src/main/java/com/committers/snowflowerthon/committersserver/CommittersServerApplication.java
+++ src/main/java/com/committers/snowflowerthon/committersserver/CommittersServerApplication.java
@@ -2,10 +2,15 @@ package com.committers.snowflowerthon.committersserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class CommittersServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/committers/snowflowerthon/committersserver/auth/github/GitHubService.java
+++ src/main/java/com/committers/snowflowerthon/committersserver/auth/github/GitHubService.java
@@ -1,12 +1,5 @@
 package com.committers.snowflowerthon.committersserver.auth.github;
 
-import com.committers.snowflowerthon.committersserver.auth.config.OAuth2MemberDto;
-import com.committers.snowflowerthon.committersserver.common.response.exception.ErrorCode;
-import com.committers.snowflowerthon.committersserver.common.response.exception.MemberException;
-import com.committers.snowflowerthon.committersserver.domain.commit.entity.Commit;
-import com.committers.snowflowerthon.committersserver.domain.commit.entity.CommitRepository;
-import com.committers.snowflowerthon.committersserver.domain.member.entity.Member;
-import com.committers.snowflowerthon.committersserver.domain.member.entity.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -22,60 +15,104 @@ import java.util.List;
 @Service
 public class GitHubService {
 
+    private final GitHubClient gitHubClient;
+
     @Transactional
     public int getRepos(String nickname) {
         RestTemplate restTemplate = new RestTemplate();
 
         int page = 1;
         int perPage = 100; // 페이지당 아이템 수 (GitHub API에서 제공하는 최대값은 100)
-        String gitHubApi = "https://api.github.com/users/%s/repos?page=%d&per_page=%d";
+//        String gitHubApi = "https://api.github.com/users/%s/repos?page=%d&per_page=%d";
 
         int totalCommits = 0; // 커밋 수
 
         while (true) {
             // GitHub API 호출 - 특정 사용자의 레포지토리 목록을 가져옴
-            ResponseEntity<List> reposResponse = restTemplate.getForEntity(
-                    String.format(gitHubApi + nickname, page, perPage),
-                    List.class);
+//            ResponseEntity<List> reposResponse = restTemplate.getForEntity(
+//                    String.format(gitHubApi, nickname, page, perPage),
+//                    List.class);
 
-            if (reposResponse.getStatusCode().is2xxSuccessful() &&reposResponse.getBody() != null) {
-                List<String> repositories = reposResponse.getBody(); // 사용자 레포 전체
+            List<RepoResponseDto> repos = gitHubClient.getRepo(nickname, page, perPage);
 
-                if (repositories.isEmpty()) { // 레포가 더 없으면 종료
-                    break;
-                }
-
-                // repositories 객체에 담긴 모든 레포에 대해 커밋 수 계산
-                totalCommits += getCommits(nickname, repositories);
-
-                // 다음 페이지로 이동
-                page++;
+            for (RepoResponseDto reponame : repos) {
+                log.info("특정 사용자의 레포지토리 이름-> {}", reponame.full_name);
             }
+            log.info("(조회 완료) 특정 사용자의 레포지토리 목록 -> {}", repos);
+
+            if (repos.isEmpty()) { // 레포가 더 없으면 종료
+                log.info("레포를 모두 조회했습니다.");
+                break;
+            }
+
+            // repositories 객체에 담긴 모든 레포에 대해 커밋 수 계산
+            totalCommits += getCommits(nickname, repos);
+
+            // 다음 페이지로 이동
+            page++;
+            log.info("다음 페이지로 이동합니다.");
+            log.info("다음 페이지: " + page);
+
+//            if (reposResponse.getStatusCode().is2xxSuccessful() &&reposResponse.getBody() != null) {
+//                List<String> repositories = reposResponse.getBody(); // 사용자 레포 전체
+//                for (String repo : repositories) {
+//                    log.info("repo name -> {}", repo);
+//                }
+//
+//                if (repositories.isEmpty()) { // 레포가 더 없으면 종료
+//                    break;
+//                }
+//
+//                // repositories 객체에 담긴 모든 레포에 대해 커밋 수 계산
+//                totalCommits += getCommits(nickname, repositories);
+//
+//                // 다음 페이지로 이동
+//                page++;
+//            }
         }
         return totalCommits;
     }
 
-    private int getCommits(String nickname, List<String> repositories) {
+    private int getCommits(String nickname, List<RepoResponseDto> repositories) {
 
-        RestTemplate restTemplate = new RestTemplate();
-        String gitHubApi = "https://api.github.com/repos/%s/%s/stats/contributors";
+//        RestTemplate restTemplate = new RestTemplate();
+//        String gitHubApi = "https://api.github.com/repos/%s/%s/stats/contributors";
+
         int totalCommits = 0;
 
-        for (String repoName: repositories) {
-            // GitHub API 호출 - 레포지토리의 기여자 통계 정보를 가져옴
-            ResponseEntity<Object[]> contributionsResponse = restTemplate.getForEntity(
-                    String.format(gitHubApi, nickname, repoName),
-                    Object[].class);
+        for (RepoResponseDto repo: repositories) {
 
-            if (contributionsResponse.getStatusCode().is2xxSuccessful() && contributionsResponse.getBody() != null) {
-                for (Object contributor : contributionsResponse.getBody()) {
-                    String contributorLogin = (String) ((HashMap<?, ?>) contributor).get("author.login");
-                    if (nickname.equals(contributorLogin)) {
-                        totalCommits += (int) ((HashMap<?, ?>) contributor).get("total");
-                    }
+//            ResponseEntity<Object[]> contributionsResponse = restTemplate.getForEntity(
+//                    String.format(gitHubApi, nickname, repoName),
+//                    Object[].class);
+//            ResponseEntity<List> contributionsResponse = restTemplate.getForEntity(
+//                    String.format(gitHubApi, nickname, repoName),
+//                    List.class);
+
+            // GitHub API 호출 - 레포지토리의 기여자 통계 정보를 가져옴
+            List<CommitResponseDto> commits = gitHubClient.getCommit(repo.full_name);
+
+            for(CommitResponseDto com : commits) {
+                log.info("커밋 sha -> {}", com.sha);
+                log.info("커미터 -> {}", com.author.login);
+                String committer = com.author.login;
+                if (committer.equals(nickname)) {
+                    totalCommits++;
                 }
             }
+
+//            if (contributionsResponse.getStatusCode().is2xxSuccessful() && contributionsResponse.getBody() != null) {
+//                for (Object contributor : contributionsResponse.getBody()) {
+//                    String contributorLogin = (String) ((HashMap<?, ?>) contributor).get("author.login");
+//                    if (nickname.equals(contributorLogin)) {
+//                        totalCommits += (int) ((HashMap<?, ?>) contributor).get("total");
+//                    }
+//                }
+//            }
         }
+
+        log.info("커밋 조회 완료");
+        log.info("총 커밋 개수는 {}개 입니다.", totalCommits);
 
         return totalCommits;
     }
