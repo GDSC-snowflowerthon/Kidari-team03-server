936fcd35b2d2e8741b08d664c6c9f34525deb22a
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-01-13 02:58:50 +0900

    🐛 CORS 오류 수정
    - 서비스 코드를 수정하였습니다.

--- src/main/java/com/committers/snowflowerthon/committersserver/auth/config/AuthenticationUtils.java
+++ src/main/java/com/committers/snowflowerthon/committersserver/auth/config/AuthenticationUtils.java
@@ -46,6 +46,14 @@ public class AuthenticationUtils {
     }
 
     private static CustomAuthenticationToken getCustomAuthenticationToken(Authentication authentication) {
-        return (CustomAuthenticationToken) authentication.getPrincipal();
+//        return (CustomAuthenticationToken) authentication.getPrincipal();
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof CustomAuthenticationToken) {
+            return (CustomAuthenticationToken) principal;
+        } else {
+            // 처리할 로직이나 예외를 처리하는 코드를 추가할 수 있습니다.
+            throw new IllegalStateException("Unexpected principal type: " + principal.getClass());
+        }
     }
 }
